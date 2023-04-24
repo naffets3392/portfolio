703033ed9a4cf711e7b46ca8a6c2f74b33a7f3fb
stefan <naffets@rocketmail.com>
2023-04-24 18:21:28 +0200

    cv image changed

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
             <h1>About me</h1>
             <h3>Hi, I'm Stefan Stevic. A Junior Front-end Developer based in Belgrade. As a Junior Front-end Developer I have skills in <span>HTML</span>, <span>CSS</span>, <span>JavaScript</span> and <span>React</span>. I'm passionate in designing websites and improving my skills, so I'm opened up for collaborations between teams in making some interesting and engaging websites in order to provide better user experience.</h3>
             <div className="links-container">
-                <a className="cvDownload" href="StefanCVsrb.pdf" download='StefanCVsrb.pdf' target="_blank" download="CV.pdf">CV Download</a>
+                <a className="cvDownload" href="StefanCVsrb.pdf" download='StefanCVsrb.pdf' target="_blank">CV Download</a>
                 <Link to='https://www.linkedin.com/in/stefan-stevic/' target="_blank"><RiLinkedinBoxFill className="icon linkedIn-icon"/></Link>
             </div>
             <Link className="home-link" to='/'>Home</Link>
